bada5a21cad5fea8eb5a6afd3df5299a8686c625
CheerfulNag <91341614+CheerfulNag@users.noreply.github.com>
2022-06-01 23:40:48 +0300

    Update UberConfig.py

--- scripts/Uber/UberConfig.py
+++ scripts/Uber/UberConfig.py
@@ -4,7 +4,7 @@ url_last = "https://auth.uber.com/login/handleanswer"
 
 headers_first = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate, br",
+    #"Accept-Encoding":"gzip, deflate, br",
     "Accept-Language":"en-US,en;q=0.5",
     "Host":"auth.uber.com",
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0",
@@ -13,7 +13,7 @@ headers_first = {
 
 headers_last = {
     "Accept":"application/json",
-    "Accept-Encoding":"gzip, deflate, br",
+    #"Accept-Encoding":"gzip, deflate, br",
     "Accept-Language":"en-US,en;q=0.5",
     "Alt-Used":"auth.uber.com",
     "Content-Type":"application/json",
