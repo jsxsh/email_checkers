19cabbe785597c2905c885a318f4e2ca20fd8e38
CheerfulNag <91341614+CheerfulNag@users.noreply.github.com>
2022-06-01 23:38:54 +0300

    Update Snagajob_28_05.py

--- scripts/Snagajob/Snagajob_28_05.py
+++ scripts/Snagajob/Snagajob_28_05.py
@@ -19,7 +19,7 @@ class checker:
                 with requests.Session() as s:
                     s.proxies.update(Proxy)
                     url_last_full = url_last+self.email
-                    r = s.get(url_last_full,headers=headers_last,timeout=10)
+                    r = s.get(url_last_full,headers=headers_last,timeout=5)
                     if r.json() == False: #Site returns "True" if email is available
                         self.result = ((self.email,"Registered"))                  
                 break
