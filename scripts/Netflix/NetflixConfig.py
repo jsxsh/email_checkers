694add2ca0f1bce80603fcdca67c3140283d3ed9
CheerfulNag <91341614+CheerfulNag@users.noreply.github.com>
2022-06-01 23:38:21 +0300

    Update NetflixConfig.py

--- scripts/Netflix/NetflixConfig.py
+++ scripts/Netflix/NetflixConfig.py
@@ -20,14 +20,14 @@ def generate_pass():
 
 headers_main = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate, br",
+    #"Accept-Encoding":"gzip, deflate, br",
     "Accept-Language":"en-US,en;q=0.5",
     "Host":"www.netflix.com",
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0"}
 
 headers_post = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate, br",
+    #"Accept-Encoding":"gzip, deflate, br",
     "Accept-Language":"en-US,en;q=0.5",
     "Content-Type":"application/x-www-form-urlencoded",
     "Host":"www.netflix.com",
