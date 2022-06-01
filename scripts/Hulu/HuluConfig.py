d5a27823376826fa661f2652ad8bb9a237ada995
CheerfulNag <91341614+CheerfulNag@users.noreply.github.com>
2022-06-01 23:31:48 +0300

    Update HuluConfig.py

--- scripts/Hulu/HuluConfig.py
+++ scripts/Hulu/HuluConfig.py
@@ -1,19 +1,14 @@
 headers_main = {
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Encoding':'gzip, deflate, br',
+    #'Accept-Encoding':'gzip, deflate, br',
     'Accept-Language':'en-US,en;q=0.5',
     'Host':'www.hulu.com',
-    #'Sec-Fetch-Dest':'document',
-    #'Sec-Fetch-Mode':'navigate',
-    #'Sec-Fetch-Site':'none',
-    #'Sec-Fetch-User':'?1',
-    #'Sec-GPC':'1',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'
 }
 
 headers_post = {
     'Accept':'application/json',
-    'Accept-Encoding':'gzip, deflate, br',
+    #'Accept-Encoding':'gzip, deflate, br',
     'Accept-Language':'en-US,en;q=0.5',
     'Content-Type':'application/x-www-form-urlencoded; charset=utf-8',
     'Host':'auth.hulu.com',
@@ -29,7 +24,7 @@ headers_post = {
 
 headers_get_csrf = {
     'Accept':'*/*',
-    'Accept-Encoding':'gzip, deflate, br',
+    #'Accept-Encoding':'gzip, deflate, br',
     'Accept-Language':'en-US,en;q=0.5',
     'Content-Type':'application/x-www-form-urlencoded; charset=utf-8',
     'Host':'secure.hulu.com',
