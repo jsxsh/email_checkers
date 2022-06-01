617e67a13a4a7f91680c4d01baa991f5396a1952
CheerfulNag <91341614+CheerfulNag@users.noreply.github.com>
2022-06-01 23:41:52 +0300

    Update WWConfig.py

--- scripts/WW/WWConfig.py
+++ scripts/WW/WWConfig.py
@@ -1,20 +1,13 @@
 
 headers_main = {
     'Accept':'application/json, text/plain, */*',
-    'Accept-Encoding':'gzip, deflate, br',
+    #'Accept-Encoding':'gzip, deflate, br',
     'Accept-Language':'en-US,en;q=0.5',
-    'Connection':'keep-alive',
-    'Content-Length':'30',
     'Content-Type':'application/json;charset=utf-8',
     'Host':'api1.weightwatchers.com',
     'Origin':'https://auth.weightwatchers.com',
     'Referer':'https://auth.weightwatchers.com/',
-    'Sec-Fetch-Dest':'empty',
-    'Sec-Fetch-Mode':'cors',
-    'Sec-Fetch-Site':'same-site',
-    'Sec-GPC':'1',
-    'TE':'trailers',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
 }
 
 
