352117902a925585894baf8689cd87c7dea78a1c
CheerfulNag <91341614+CheerfulNag@users.noreply.github.com>
2022-06-04 04:25:54 +0300

    Update imports.py

--- shared/imports.py
+++ shared/imports.py
@@ -1,31 +1,33 @@
 
+
 class Importer:
     def __init__(self,name):
         if name == "Amazon":
-            from scripts.Amazon.Amazon_28_05 import checker
-            object = checker
+            from scripts.Amazon.Amazon import Checker
+            object = Checker
         if name == "BK":
-            from scripts.BK.BK_28_05 import checker
-            object = checker
+            from scripts.BK.BK import Checker
+            object = Checker
         if name == "Hulu":
-            from scripts.Hulu.Hulu_28_05 import checker
-            object = checker
+            from scripts.Hulu.Hulu import Checker
+            object = Checker
         if name == "Indeed":
-            from scripts.Indeed.Indeed_28_05 import checker
-            object = checker
+            from scripts.Indeed.Indeed import Checker
+            object = Checker
         if name == "LinkedIn":
-            from scripts.LinkedIn.LinkedIn_28_05 import checker
-            object = checker
+            from scripts.LinkedIn.LinkedIn import Checker
+            object = Checker
         if name == "Netflix":
-            from scripts.Netflix.Netflix_28_05 import checker
-            object = checker
+            from scripts.Netflix.Netflix import Checker
+            object = Checker
         if name == "Snagajob":
-            from scripts.Snagajob.Snagajob_28_05 import checker
-            object = checker
+            from scripts.Snagajob.Snagajob import Checker
+            object = Checker
         if name == "Uber":
-            from scripts.Uber.Uber_28_05 import checker
-            object = checker
+            from scripts.Uber.Uber import Checker
+            object = Checker
         if name == "WW":
-            from scripts.WW.WW_28_05 import checker
-            object = checker
+            from scripts.WW.WW import Checker
+            object = Checker
         self.checker = object
+
